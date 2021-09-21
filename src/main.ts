f1977d2a85818aab8da14737b8ac1ff53747bb37
hectorvcode <hectorfabiovasquez@gmail.com>
2021-09-21 11:39:38 -0500

    Added Heroku configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -20,6 +21,10 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(3000);
+  //await app.listen(3000);
+  app.enableCors();
+  const PORT = parseInt(process.env.PORT,10) || 3000;
+  Logger.log(`app running on: ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
