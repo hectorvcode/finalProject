e055840e3d06dbbd33e364a14e452c9eadc07e00
hectorvcode <hectorfabiovasquez@gmail.com>
2021-09-22 11:14:51 -0500

    Changed Service Method GET

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -10,8 +10,9 @@ export class ProductService {
     constructor(@InjectModel('Product') private readonly productModel: Model<IProduct>){}
 
     async getProducts(): Promise<IProduct[]>{
-        const products = await this.productModel.find();
-        return products;
+
+        return await this.productModel.find();
+
     }
 
     async getProduct(productID:string): Promise<IProduct>{
