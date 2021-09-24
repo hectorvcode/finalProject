7ab724049a75b6479a35e10470273f61b4a66dd4
hectorvcode <hectorfabiovasquez@gmail.com>
2021-09-24 18:37:02 -0500

    Added CORS policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = parseInt(process.env.PORT,10) || 3000;
 
   const options = new DocumentBuilder()
