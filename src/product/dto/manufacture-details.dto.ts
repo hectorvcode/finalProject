89160c28be9d4851b08ba765375943caa9080609
hectorvcode <hectorfabiovasquez@gmail.com>
2021-09-26 11:28:46 -0500

    Added nested documents details on documentation

--- src/product/dto/manufacture-details.dto.ts
+++ src/product/dto/manufacture-details.dto.ts
@@ -1,11 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 import { IsString } from "class-validator";
 import { IManufactureDetails } from "../interfaces/manufacture_detail.interface";
 
 
 export class ManufactureDetailsDTO implements IManufactureDetails {
+    
+    @Type( ()=>String ) 
+    @ApiProperty({ type:String, description:'This is the product\'s model' })
     @IsString()
     model:string;
 
+    @Type( ()=>String ) 
+    @ApiProperty({ type:String, description:'This is the product\'s brand'})
     @IsString()
     brand:string;
 }
--- src/product/dto/shipping-details.dto.ts
+++ src/product/dto/shipping-details.dto.ts
@@ -1,16 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 import { IsNumber } from "class-validator";
 import { IShipping_Details } from "../interfaces/shipping_details.interface";
 
 export class ShippingDetailsDto implements IShipping_Details {
-    @IsNumber()
-    weight: number;
+    
+  @Type(()=>Number)
+  @ApiProperty({ type:Number, description:'This is the product\'s weight'})
+  @IsNumber()
+  weight: number;
   
-    @IsNumber()
-    width: number;
+  @Type(()=>Number)
+  @ApiProperty({ type:Number, description:'This is the product\'s width'})
+  @IsNumber()
+  width: number;
   
-    @IsNumber()
-    height: number;
+  @Type(()=>Number)
+  @ApiProperty({ type:Number, description:'This is the product\'s height'})
+  @IsNumber()
+  height: number;
   
-    @IsNumber()
-    depth: number;
+  @Type(()=>Number)
+  @ApiProperty({ type:Number, description:'This is the product\'s depth'})
+  @IsNumber()
+  depth: number;
   }
